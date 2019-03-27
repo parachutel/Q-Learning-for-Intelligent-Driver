e4e71ac4dffdfc63d8ef80d1feadd19e2b034233
Sheng Li <lisheng@stanford.edu>
2019-03-28 00:42:07 +0800

    Update main_evaluate.m

--- main_evaluate.m
+++ main_evaluate.m
@@ -1,11 +1,14 @@
 clear;
 
+# Change to the policy (Q-table) name to be evaluated (.csv):
+policy_file_name = 'Q_alpha_0.2_episode_10000_w2_10_w5_0.1_epsilon_0.1.csv'
+
 cd Parameters
 %%%%%%% Read Policy_1 from a Q file:
-Q = csvread('Q_alpha_0.2_episode_10000_w2_10_w5_0.1_epsilon_0.1.csv');
+Q = csvread(policy_file_name);
 [~, Policy_1] = max(Q,[],2);
 %%%%%%% OR read Policy_1 from a policy file:
-% Policy_1 = csvread('Policy_0_refined.csv');
+% Policy_1 = csvread(policy_file_name);
 % Policy_1 = Policy_1(:);
 cd ..
 
@@ -23,5 +26,5 @@ max_car_num      = 32;
     evaluate_policy(Policy_1, plot_flag, if_saving_gif, ...
                     simu_times, simulate_horizon, min_car_num, max_car_num);
 
-% fprintf('\nCollision rate = %3.2f %% \n', colli_rate*100 );
-% fprintf('Performance    = %3.2f \n', performance);
+fprintf('\nCollision rate = %3.2f %% \n', colli_rate*100);
+fprintf('Performance    = %3.2f \n', performance);
